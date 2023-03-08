0c0b82f3dd630edae7893c727f5a5602a48e6332
Thor the Fixer <120442441+Jaymasters440@users.noreply.github.com>
2023-03-07 17:10:47 -0800

    Create  README and start build 4 Index.js

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
-// Create the employee category
-function employee(name, id, email, getName, getId, getEmail, getRole) {
-    this.name
-    this.id
-    this.email
-    this.getName
-    this.getId
-    this.getEmail
-    this.getRole
-    this.role = employee () 
-        console.log(Employee)
+const inquirer = require('inquirer');
+const fs = require("fs");
+const gm = require("./utils/generateHTML.js");
 
-    };
-            
+// generate Questions for each type of employee
+var managerQuestions = [{
+    type: "input",
+    message: "What is the manager's name?",
+    name: "name"
+},
+
+{
+    type: "input",
+    message: "What is the manager's id?",
+    name: "id"
+},
+
+{
+    type: "input",
+    message: "What is the manager's email?",
+    name: "email"
+},
+
+{
+    type: "input",
+    message: "What is the manager's office?",
+    name: "office",
+},
+
+]
+// Function to write to HTML
 
 
-// Use Employe category and allow Manager to inherit employee properties and add officeNumber 
-function Manager(name, id, email, getName, getId, getEmail, getRole) {
-    employee.call(this, name, id, email, getName, getId, getEmail, getRole)
-    this.manager = this.manager;
-//Figure out how to call Manager
-    console.log
-}
 
-// Use Employe category and allow Engineer to inherit employee properties and add github and GitHub username
-function Engineer(name, id, email, getName, getId, getEmail, getRole) {
-    employee.call(this, name, id, email, getName, getId, getEmail, getRole)
-    this.engineer = this.engineer;
+// Function to initialize app //??
+function init() {
+    inquirer
+    .prompt(questions)
 
-// Figure out how to call Engineer
-    console.log
-}
-// Use Employe category and allow Intern to inherit employee properties and add school and getSchool
-function Intern(name, id, email, getName, getId, getEmail, getRole) {
-    employee.call(this, name, id, email, getName, getId, getEmail, getRole)
-    this.intern = this.intern;
-
-// Figure out how to call Engineer
-    console.log
-}
+    .then((data) => {
+        const filename = ``
+        writeToFile(filename, data);
+    });
+ }
