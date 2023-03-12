49784bf563727605c1aee8fccde92cb4db5f7d34
Thor the Fixer <120442441+Jaymasters440@users.noreply.github.com>
2023-03-12 16:54:15 -0700

    Add manager test

--- tests/employee.test.js
+++ tests/employee.test.js
@@ -1,8 +1,7 @@
 const { Employee, Manager, Engineer, Intern } = require("./lib/employee.js");
 
 describe('Employee', () => {
-    // A test is created to check that modulus does in fact return the remainder of the quotient of the two numbers.
-    // This test checks to see if 2 % 2 has a remainder of 0 and returns 0.
+    // A test is created to check that modulus does in fact return the the getName sent to the constructor employee.
     describe('getName', () => {
       it('should return the name sent to the constructor', () => {
         const name = "AnyName";
---
+++ tests/manager.test.js
@@ -0,0 +1,12 @@
+const { Employee, Manager, Engineer, Intern } = require("./lib/employee.js");
+
+describe('Manager', () => {
+    // A test is created to check that modulus does in fact return the the getName sent to the constructor Manager.
+    describe('getName', () => {
+      it('should return the name sent to the constructor', () => {
+        const name = "AnyName";
+        const manager = new Manager(name, 1235, "Randome.com");
+        expect(manager.getName()).toEqual(name);
+      });
+    });
+  });
